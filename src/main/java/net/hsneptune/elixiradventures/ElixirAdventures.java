3bf41838336f6efcbb1d6f7ab1db969ee89d5fdb
TheManeeter <maneet.singh863@outlook.com>
2024-02-03 16:33:05 -0500

    License Removal

--- src/main/java/net/hsneptune/elixiradventures/ElixirAdventures.java
+++ src/main/java/net/hsneptune/elixiradventures/ElixirAdventures.java
@@ -17,6 +17,6 @@ public class ElixirAdventures implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello!");
 	}
 }
