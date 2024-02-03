d1bb10020a85d44eefd0a29f5a819ec8be30a5dd
TheManeeter <maneet.singh863@outlook.com>
2024-02-03 17:14:25 -0500

    Testing

--- src/main/java/net/hsneptune/elixiradventures/ElixirAdventures.java
+++ src/main/java/net/hsneptune/elixiradventures/ElixirAdventures.java
@@ -17,6 +17,6 @@ public class ElixirAdventures implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("test");
+		LOGGER.info("test1");
 	}
 }
